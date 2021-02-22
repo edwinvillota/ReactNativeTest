5a2cd0cc2483bff69108ec981ba625979dee6b7d
edwin <edwin@pop-os.localdomain>
2021-02-22 10:50:33 -0500

    User enviroment variables and remove literals

--- src/redux/api/usersApi.js
+++ src/redux/api/usersApi.js
@@ -1,6 +1,8 @@
+import Config from 'react-native-config';
+
 export const getAllUsers = async () => {
   try {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await fetch(Config.USERS_API_URL);
     const data = await users.json();
     return data;
   } catch (err) {
